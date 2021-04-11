520c6cd37af631e9b3e0a548193d95f2d77fd983
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-11 12:35:59 +0200

    nenw branch is already done to use simple panel

--- gam.py
+++ gam.py
@@ -1,13 +1,25 @@
 
-
-
-
 def panel():
+    print("[1] => Info About Application <= ")
+    print("[2] => SnakeGame 1s game <= ")
+    print("[3] => SecretGame 2s game <= ")
+    print("[4] => Info About files/directories <= ")
     print(" ")
-
 
 def main():
    print("WELCOME ON PANEL ABOUT GAME IN WINDOW")
    print(" ")
    panel()
+
+   way = int(input("Your choice from menu ==> "))
+
+   if way == 1:
+       pass
+   elif way == 2:
+       pass
+   elif way == 3:
+       pass
+   elif way == 4:
+       pass
+
 main()
