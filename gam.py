8703ec4c51bdebee32283dd61704fa88e2223ae7
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-11 12:29:34 +0200

    push simple functions before new branch

--- gam.py
+++ gam.py
@@ -1,1 +1,16 @@
-print("WELCOME ON PANEL ABOUT GAME IN WINDOW")
+
+
+
+
+
+
+def panel():
+    print(" ")
+
+
+
+def main():
+   print("WELCOME ON PANEL ABOUT GAME IN WINDOW")
+   print(" ")
+   panel()
+main()
