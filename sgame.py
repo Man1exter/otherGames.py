ef104205fa7b670438f57493dbc8a875dbc39911
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-14 10:42:37 +0200

    change to variables on w/h

--- sgame.py
+++ sgame.py
@@ -4,12 +4,16 @@ import sys
 def seek():
     print(pygame.__version__)
     screen = pygame.display.set_mode((1280,720))
+
     # player = pygame.image.load("./Programmer Backgrounds 15.jpg")
 
+    player_width = 50
+    player_height = 50
+    
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
 
-        pygame.draw.rect(screen,(13,255,0),pygame.Rect(10,50,75,75))
+        pygame.draw.rect(screen,(13,255,0),pygame.Rect(10,50,player_width,player_height))
         pygame.display.flip()
