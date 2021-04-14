e57d10122fe9f83005c026f82d14a5c7d73a5539
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-14 10:23:49 +0200

    show screen with loop

--- sgame.py
+++ sgame.py
@@ -1,4 +1,11 @@
 import pygame
+import sys
+
+screen = pygame.display.set_mode((1280,720))
 
 def seek():
     print(pygame.__version__)
+    while True: 
+        for event in pygame.event.get():
+            if event.type ++ pygame.QUIT:
+                sys.exit(0)
