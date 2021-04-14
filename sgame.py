e2643af8f4d3a8da4bd5ab2265783e80b36ca606
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-14 10:40:55 +0200

    resize width/height on rect

--- sgame.py
+++ sgame.py
@@ -4,11 +4,12 @@ import sys
 def seek():
     print(pygame.__version__)
     screen = pygame.display.set_mode((1280,720))
+    # player = pygame.image.load("./Programmer Backgrounds 15.jpg")
 
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
 
-        pygame.draw.rect(screen,(0,150,200),pygame.Rect(10,50,150,200))
+        pygame.draw.rect(screen,(13,255,0),pygame.Rect(10,50,75,75))
         pygame.display.flip()
