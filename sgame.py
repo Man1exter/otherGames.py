c428a1fe445000b2d59b3f7ad1e44ba9e7e1107b
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-14 10:46:50 +0200

    apl keyboard keys to move on screen

--- sgame.py
+++ sgame.py
@@ -9,7 +9,7 @@ def seek():
 
     player_width = 50
     player_height = 50
-    
+
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,3 +17,14 @@ def seek():
 
         pygame.draw.rect(screen,(13,255,0),pygame.Rect(10,50,player_width,player_height))
         pygame.display.flip()
+
+def keyboard():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        pass # += 1
+    if keys[pygame.K_d]:
+        pass # s += y
+    if keys[pygame.K_d]:
+        pass # w -= y
+    if keys[pygame.K_d]:
+        pass # a -= x
