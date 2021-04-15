f621f2148ccd0b97579bb18ebbe15b9d5ce15e73
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-15 11:05:44 +0200

    change background image

--- sgame.py
+++ sgame.py
@@ -2,11 +2,12 @@ import pygame
 import sys
 
 def seek():
+    pygame.init()
     print(pygame.__version__)
     screen = pygame.display.set_mode((1280,720))
     pygame.display.set_caption("WELCOME TO THE JUNGLE...")
 
-    background = pygame.image.load("./Programmer Backgrounds 15.jpg")
+    background = pygame.image.load("./Programmer Backgrounds 15.jpg").convert()
     player_width = 50
     player_height = 50
     box = pygame.Rect(10,10,55,55)
@@ -16,7 +17,6 @@ def seek():
             if event.type == pygame.QUIT:
                 sys.exit(0)
 
-        screen.blit(background, [0, 0])
 
         #moving..
         keys = pygame.key.get_pressed()
@@ -30,7 +30,8 @@ def seek():
           box.x -= 1
 
         #drawing...
-        screen.fill((0,0,0))
+        screen.blit(background, [0, 0])
+        #screen.fill((0,0,0))
         pygame.draw.rect(screen,(13,255,0),pygame.Rect(10,50,player_width,player_height))
         pygame.display.flip()
 
