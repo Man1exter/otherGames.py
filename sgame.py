90284080e023eda810c733d6521eec9d00d403e5
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-15 11:41:43 +0200

    moving is completly done

--- sgame.py
+++ sgame.py
@@ -4,12 +4,12 @@ import sys
 def seek():
     print(pygame.__version__)
     screen = pygame.display.set_mode((1280,720))
-    box = pygame.Rect(10,10,50,50)
 
     background = pygame.image.load("./Programmer Backgrounds 15.jpg").convert()
-
+    
     player_width = 50
     player_height = 50
+    box = pygame.Rect(10,10,player_width,player_height)
 
     while True: 
         for event in pygame.event.get():
@@ -29,17 +29,5 @@ def seek():
 
         #drawing...
         screen.blit(background, [0, 0])
-        #screen.fill((0,0,0))
-        pygame.draw.rect(screen,(13,255,0),pygame.Rect(10,50,player_width,player_height))
+        pygame.draw.rect(screen,(13,255,0),box)
         pygame.display.flip()
-
-def keyboard():
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        pass # d += 1
-    if keys[pygame.K_d]:
-        pass # s += y
-    if keys[pygame.K_d]:
-        pass # w -= y
-    if keys[pygame.K_d]:
-        pass # a -= x
