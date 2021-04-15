6441e96a6399da40da5681ab6e56029426071f19
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-15 11:03:20 +0200

    change title of the app [2]

--- sgame.py
+++ sgame.py
@@ -4,21 +4,21 @@ import sys
 def seek():
     print(pygame.__version__)
     screen = pygame.display.set_mode((1280,720))
+    pygame.display.set_caption("WELCOME TO THE JUNGLE...")
 
-    # player = pygame.image.load("./Programmer Backgrounds 15.jpg")
-
+    background = pygame.image.load("./Programmer Backgrounds 15.jpg")
     player_width = 50
     player_height = 50
-    box = pygame.Rect(10,50,player_width,player_height)
+    box = pygame.Rect(10,10,55,55)
 
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
 
-        pygame.draw.rect(screen,(13,255,0),pygame.Rect(10,50,player_width,player_height))
-        pygame.display.flip()
+        screen.blit(background, [0, 0])
 
+        #moving..
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
           box.x += 1
@@ -28,3 +28,10 @@ def seek():
           box.y -= 1
         if keys[pygame.K_d]:
           box.x -= 1
+
+        #drawing...
+        screen.fill((0,0,0))
+        pygame.draw.rect(screen,(13,255,0),pygame.Rect(10,50,player_width,player_height))
+        pygame.display.flip()
+
+        
