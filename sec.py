209677769807a981b158a7240b661b0f38a4bd96
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-21 20:38:59 +0200

    delete space (empty)

--- sec.py
+++ sec.py
@@ -1,10 +1,18 @@
 import pygame
 import sys
-
 class Game(object):
+
+    # initialization the all constructs the game...
     def __init__(self):
         pass
+ 
+    # ticking the tps and other...
+    def tick(self):
+        pass
 
+    # drawing elements on the screen..
+    def drawing(self):
+        pass
 
 if __name__ == "__main__":
     Game()
