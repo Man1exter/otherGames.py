8faf298462d4c835c9108872fa4218b34980a31a
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-29 13:44:34 +0200

    add 2 more class with player and reward -> beer

--- sec.py
+++ sec.py
@@ -14,5 +14,36 @@ class Game(object):
     def drawing(self):
         pass
 
+
+class Player(object):
+
+    # initialization the all constructs the game...
+    def __init__(self):
+        pass
+ 
+    # ticking the tps and other...
+    def tick(self):
+        pass
+
+    # drawing elements on the screen..
+    def drawing(self):
+        pass
+
+
+class Beer(object):
+    
+    # initialization the all constructs the game...
+    def __init__(self):
+        pass
+ 
+    # ticking the tps and other...
+    def tick(self):
+        pass
+
+    # drawing elements on the screen..
+    def drawing(self):
+        pass
+
+
 if __name__ == "__main__":
     Game()
