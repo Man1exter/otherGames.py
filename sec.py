4b73da2328bfdea1897600145acaac538b848e80
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-30 09:30:08 +0200

    change position and add self...

--- sec.py
+++ sec.py
@@ -7,49 +7,31 @@ class Game(object):
         pygame.init()
         pygame.font.init()
 
+        self.max_tps = 75.0
+        self.screen = pygame.display.set_mode((1440,900)) # 1440×900 (16:10)
+        self.clock = pygame.time.Clock()
+        self.delta_tps = 0.0
+
+        while self.delta > 1 / self.max_tps:
+              self.delta -= 1 / self.max_tps
+
         while True: 
          for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
+
+            # ticking...
+
+            # drawing...
+            self.background = pygame.image.load("./Programmer Backgrounds 15.jpg").convert()
+            self.screen.blit(self.background, (0, 0))
+            pygame.display.flip()
+
  
     # ticking the tps and other...
     def tick(self):
-        max_tps = 70.0
-        clock = pygame.time.Clock()
-        delta = 0.0
 
-        delta = clock.tick()
-        while delta > 1 / max_tps:
-              delta -= 1 / max_tps
-
-    # drawing elements on the screen..
-    def drawing(self):
-        screen = pygame.display.set_mode((1440,900)) # 1440×900 (16:10)
-        background = pygame.image.load("./Programmer Backgrounds 15.jpg").convert()
-
-        player_width = 70
-        player_height = 70
-        box = pygame.Rect(10,10,player_width,player_height)
-
-        screen.blit(background, (0, 0))
-        pygame.draw.rect(screen,(13,255,0),box)
-        pygame.display.flip()
-
-
-class Player(object):
-
-    # initialization the all constructs the game...
-    def __init__(self):
-        pass
- 
-    # ticking the tps and other...
-    def tick(self):
-        pass
-
-    # drawing elements on the screen..
-    def drawing(self):
-
-        # ..moving WASD from keyboard..
+         # ..moving WASD from keyboard..
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
@@ -72,21 +54,9 @@ class Player(object):
         if keys[pygame.K_LEFT]:
           self.x -= 1
 
-
-class Beer(object):
-    
-    # initialization the all constructs the game...
-    def __init__(self):
-        pass
- 
-    # ticking the tps and other...
-    def tick(self):
-        pass
-
     # drawing elements on the screen..
     def drawing(self):
-        pass
-
+        pygame.draw.rect(self.screen,(13,255,0),self.box)
 
 if __name__ == "__main__":
     Game()
